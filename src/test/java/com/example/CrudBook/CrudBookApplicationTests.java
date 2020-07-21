b4cfa9b5f3c2fce94ecb2b6b0c8f72bd15c1e33e
callthecops <tudor_rimbetiu@yahoo.com>
2020-07-21 22:06:58 +0300

    Init

---
+++ src/test/java/com/example/CrudBook/CrudBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrudBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
